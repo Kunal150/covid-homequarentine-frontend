5a379c70a753e0b0c94f9424631a2ce4bcf75249
Kunal150 <67556367+Kunal150@users.noreply.github.com>
2021-05-20 19:08:41 +0530

    Create Recommended.js

---
+++ Recommended.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import "./Recommended.css";
+
+
+function Recommended() {
+    return (
+        <div className="recommended">
+        <div className="recommended__info">
+     <h1>Best Doctors in Bhopal for Home Quarentine patient </h1>
+
+        </div>
+        <img className="recommended__image"
+        src="https://wallpaperaccess.com/full/619994.jpg" />
+      <div className="tareascore">
+     <ul>
+     <li>
+     Dr. Vikas Mishra
+Pulmonary Medicine, MBBS
+
+Pulmonologist
+:Arera Colony, Bhopal
+Asthma Arogyam
+
+E-7/163, Landmark: Opposite Mazar & Near Apna Namkeen, Bhopal
+     </li>
+     <li>
+     Dr. Ravindra Gulati:Address: E-6/41, E-6, Arera Colony, Bhopal, Madhya Pradesh 462016
+     </li>
+     <li>
+     Dr. Rashmi Verma
+Consultant -- Internal Medicine & Rheumotology
+
+MBBS, MD, SR ( Internal Medicine)
+Bansal Hospital, C - Sector, Shahapura Bhopal, M.P. 462016, India
++91-0755-4086000
++91-0755-4086099
+     </li>
+     <li>
+     Dr. Rajeev Madan Address: 59-B, Near Chetak bridge, Sector B, Kasturba Nagar, Bhopal, Madhya Pradesh 462023
+Hours: 
+Open ⋅ Closes 5PM
+Phone: 0755 427 1638
+     </li>
+     
+ 
+    
+    </ul>
+      </div>
+            
+        </div>
+    )
+}
+
+export default Recommended

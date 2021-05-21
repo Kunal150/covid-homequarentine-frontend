738023263b848cdff3a84e310b5158c693b2cd2f
Kunal150 <67556367+Kunal150@users.noreply.github.com>
2021-05-21 15:27:48 +0530

    Create Medical.js

---
+++ client/src/Medical.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import "./Medical.css";
+function Medical() {
+    return (
+        <div className="medical">
+        <div className="medical__info">
+     <h1>Medical store in bhopal that will provide home delivery medicine to covid patient </h1>
+
+        </div>
+        <img className="medical__image"
+        src="https://i.ytimg.com/vi/nYeNPZHtb4A/maxresdefault.jpg" />
+      <div className="ztareascore">
+     <ul>
+     <li>
+     Amit Medical Stores
+Pharmacy
+098273 61966
+Open ⋅ Closes 10PM
+Lalita Nagar, Kolar Rd, Bhopal, Madhya Pradesh 462042
+     </li>
+     <li>
+     Maheshwari Medical Store
+Address: 3, Yesoda Parisar Op Bima Kunje, Kolar Road, Kolar Road, Bhopal, Madhya Pradesh 462016
+Phone: 0755 424 5833
+     </li>
+     <li>
+     Medical store
+Medical supply store in Madhya Pradesh
+Address: shop no 9 Girdhar Hight 2, Kolar Rd, near vidhan aleena, Bhopal, Madhya Pradesh 462042
+Hours: 
+Open ⋅ Closes 9PM
+
+     </li>
+     <li>
+     Anupam Medical Store
+
+Address: Kolar Rd, Fine Campus, Aashirwad Colony, Kolar Rd, Bhopal, Madhya Pradesh 462042
+Hours: 
+Open ⋅ Closes 10PM
+Phone: 077709 07699
+     </li>
+     
+ 
+    
+    </ul>
+      </div>
+            
+        </div>
+    )
+}
+
+export default Medical
